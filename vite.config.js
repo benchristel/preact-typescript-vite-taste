bec568f9c8d7b4971f70728833d3fbf220633207
Ben Christel <ben.christel@gmail.com>
2022-10-01 13:54:52 -0700

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import {defineConfig} from "vite"
 
 export default defineConfig({
+  // Base path for references to asset files in the built code.
+  // Setting this to empty causes assets to be imported from a
+  // relative path, which is necessary if your app is deployed
+  // to a subdirectory (e.g. https://benchristel.github.io/my-app/)
+  base: "",
   esbuild: {
     jsxFactory: "h",
     jsxFragment: "Fragment",
