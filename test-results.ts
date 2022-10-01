680c6b24f96038cd3ddbe9ab20cf3ce4ded5fed1
Ben Christel <ben.christel@gmail.com>
2022-09-30 21:28:21 -0700

    Refactor test-results.ts

--- test-results.ts
+++ test-results.ts
@@ -1,21 +1,15 @@
 if ((import.meta as any).env.DEV) {
-  import("@benchristel/taste").then(
-    ({ getAllTests, runTests, formatTestResultsAsText }) => {
-      runTests(getAllTests())
-        .then(formatTestResultsAsText)
-        .then((results: string) => {
-          const resultsElement = document.getElementById("test-results");
-          if (resultsElement == null) {
-            throw Error("test-results element not found");
-          }
-
-          resultsElement.innerText = results;
-          if (results.includes("fail")) {
-            resultsElement.setAttribute("class", "fail");
-          } else {
-            resultsElement.setAttribute("class", "pass");
-          }
-        });
-    }
-  );
+  import("@benchristel/taste")
+    .then(({getAllTests, runTests, formatTestResultsAsText}) =>
+      runTests(getAllTests()).then(formatTestResultsAsText),
+    )
+    .then((results: string) => {
+      const resultsElement = document.getElementById("test-results")
+      resultsElement!.innerText = results
+      resultsElement!.setAttribute(
+        "class",
+        results.includes("fail") ? "fail" : "pass",
+      )
+    })
+    .catch(console.error)
 }
