81d8177ed1313ecd5628ec6c4e87a6e7dc6104c8
Ben Christel <ben.christel@gmail.com>
2022-10-04 21:35:02 -0700

    Add preact/compat alias for react

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,12 @@ export default defineConfig({
     jsxFactory: "h",
     jsxFragment: "Fragment",
   },
+  resolve: {
+    alias: {
+      "react": "preact/compat",
+      "react-dom/test-utils": "preact/test-utils",
+      "react-dom": "preact/compat", // Must be below test-utils
+      "react/jsx-runtime": "preact/jsx-runtime",
+    },
+  },
 })
